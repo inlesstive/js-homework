6841c441c7a76ad60fbf09e93aa489e14445d91f
inlesstive <inlesstive@gmail.com>
2023-08-04 14:45:11 +0500

    done.

---
+++ index_3.js
@@ -0,0 +1,11 @@
+let a = +prompt('Введите число');
+let b = +prompt('Введите число');
+let c = +prompt('Введите число');
+
+
+
+if((a > b && a < c) || (a > c && a < b)){
+    alert(a)
+} else {
+    alert(b)
+}
